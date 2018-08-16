b6705ff60b212b126527822bc72abb4b14eab703
Karen Justo <40191282+JR-Karen9@users.noreply.github.com>
2018-08-16 10:50:22 -0300

    Get user input and calculate the result

--- js/script.js
+++ js/script.js
@@ -27,25 +27,57 @@ function factorial(num) {
 }
 
 function operate(a,b,op){
-	return op(a,b);
+	switch(op){
+		case '×':
+			return multiply(a,b)
+		case '/':
+			return divide(a,b)
+		case '+':
+			return add(a,b)
+		case '-':
+			return subtract(a,b)
+	}
 }
 
 function printDisplay(){
 	let input = this.textContent;
 	display.textContent += input;
 
-	if(number.test(input)){
+	if(number.test(input))
 		userInput += input;
-	}
-	else{
+	else
 		userInput += ' ' + input + ' ';
-		console.log(userInput);
-	} 
-		
 }
 
 function result(){
-
+	userInput = userInput.split(' ');
+	let total = 0;
+	let i = undefined;
+	let finished = false;
+	while(!finished){
+		if(userInput.includes('×')){
+			i = userInput.findIndex((element) => element == '×');
+		}
+		else if(userInput.includes('/')){
+			i = userInput.findIndex((element) => element == '/');
+		}
+		else if(userInput.includes('+')){
+			i = userInput.findIndex((element) => element == '+');
+		}
+		else if(userInput.includes('-')){
+			i = userInput.findIndex((element) => element == '-');
+		}
+		total = operate(parseInt(userInput[i-1]), parseInt(userInput[i+1]), userInput[i]);
+		userInput.splice(i-1, 3, total);
+		console.log(userInput);
+		if(userInput.includes(NaN)){
+			total = 'Incorrect math expression';
+			break;
+		}
+		if(userInput.length == 1)
+			finished = true;
+	}
+	display.textContent = total;
 }
 
 const display = document.querySelector('.display p');
@@ -57,5 +89,5 @@ numbers.forEach(number => number.addEventListener('click', printDisplay));
 const operations = document.querySelectorAll('.operation');
 operations.forEach(operation => operation.addEventListener('click', printDisplay));
 
-const equal = document.querySelector('#equal');
-//equal.addEventListener('click', result);
+const equal = document.querySelector('#equals');
+equal.addEventListener('click', result);
